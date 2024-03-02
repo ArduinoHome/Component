d13dbf5f2e7289f13d0a6b1e2b474faf1ad75d87
LukeScrewdriver <luke.screwdriver@gmail.com>
2024-03-02 17:43:44 +0100

    version 05.02.00
     - added timer stop

--- tool/timer/Timer.cpp
+++ tool/timer/Timer.cpp
@@ -7,17 +7,25 @@ void Timer::Start(unsigned long interval, bool reset = false)
     this->interval = interval;
     this->reset = reset;
     this->startTime = millis();
+    enabled = true;
+}
+
+void Timer::Stop()
+{
+    enabled = false;
 }
 
 bool Timer::Elapsed()
 {
     bool result = false;
 
-    if ((millis() - startTime) >= interval)
+    if ((millis() - startTime) >= interval && enabled == true)
     {
         result = true;
         if (reset)
             startTime = millis();
+        else
+            enabled = false;
     }
 
     return result;
--- tool/timer/Timer.h
+++ tool/timer/Timer.h
@@ -9,11 +9,13 @@ private:
     unsigned long startTime;
     unsigned long interval;
     bool reset;
+    bool enabled =false;
 
 public:
     Timer();
     void Start(unsigned long interval, bool reset);
     bool Elapsed();
+    void Stop();
 };
 
 #endif
